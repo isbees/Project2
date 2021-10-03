7e09a3e0792258d7dba1fb2f6b3ef299bc3eb66f
isbees <isbeesbrukhman@gmail.com>
2021-10-03 12:34:14 -0400

    Test2

--- src/uintTesting/Person.java
+++ src/uintTesting/Person.java
@@ -2,5 +2,5 @@ package uintTesting;
 
 public class Person {
     public Person(){}
-    int a =5;
+    int a = 69;
 }
--- src/uintTesting/Resident.java
+++ src/uintTesting/Resident.java
@@ -1,4 +1,5 @@
 package uintTesting;
 
 public class Resident {
+    public Resident(){}
 }
