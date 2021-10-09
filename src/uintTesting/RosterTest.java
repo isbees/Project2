5e2599f6be40dd30297f4d33da8f91b4a3e817ce
Zach213 <78453852+Zach213@users.noreply.github.com>
2021-10-09 14:42:29 -0400

    Added tests for remove

--- src/uintTesting/RosterTest.java
+++ src/uintTesting/RosterTest.java
@@ -7,7 +7,6 @@ import static org.junit.Assert.*;
 public class RosterTest {
 
     //Able to add to empty, add to 1, add to 2 for tristate, resident, nonresident, and international
-    //Also maybe test adding a few diff types together
     @Test
     public void add() {
         //Test 1 International: (.1->.3) -> Add to empty, 1, and 2.
@@ -96,16 +95,67 @@ public class RosterTest {
         Profile resProfile = new Profile("Hughe Landough", "IT", true);
         Resident resStudent = new Resident(resProfile, 14);
         assertTrue(test5Roster.add(resStudent));
-
     }
 
+    //Able remove from 2 or 1, but not from empty for tristate, resident, nonresident, and international
     @Test
     public void remove() {
-        //Unable to remove from empty
+        //Making usable international, nonresident, tristate, resident, and student.
+        Profile interProfile = new Profile("John Adams", "CS", true);
+        International interStudent = new International(interProfile, 14);
+        Profile triProfile = new Profile("Mark Sams", "IT", true);
+        TriState triStudent = new TriState(triProfile, 14);
+        Profile nonResProfile = new Profile("Paul Jeff", "CS", true);
+        NonResident nonResStudent = new NonResident(nonResProfile, 14);
+        Profile resProfile = new Profile("Andy Can", "CS", true);
+        NonResident resStudent = new NonResident(resProfile, 14);
+        Student testStudent = new Student("Landon Ades", "CS", 15);
 
-        //Able to remove from roster of 1
 
-        //Able to remove from roster of 2
+        //Test 1 - Unable to remove any type of student from an empty roster
+        Roster test1 = new Roster();
+        assertFalse(test1.remove(interStudent));
+        assertFalse(test1.remove(triStudent));
+        assertFalse(test1.remove(nonResStudent));
+        assertFalse(test1.remove(resStudent));
+        assertFalse(test1.remove(testStudent));
 
+        //Test 2 - Unable to remove a student not in the roster, can remove the student in the roster
+        Roster test2 = new Roster();
+        test2.add(testStudent);
+        assertFalse(test2.remove(interStudent));
+        assertFalse(test2.remove(triStudent));
+        assertFalse(test2.remove(nonResStudent));
+        assertFalse(test2.remove(resStudent));
+        assertTrue(test2.remove(testStudent));
+
+        //Test 3 - Can remove any type of student from the roster if they're in there - resStudent
+        Roster test3 = new Roster();
+        test3.add(resStudent);
+        assertFalse(test3.remove(interStudent));
+        assertFalse(test3.remove(triStudent));
+        assertFalse(test3.remove(nonResStudent));
+        assertTrue(test3.remove(resStudent));
+        assertFalse(test3.remove(testStudent));
+
+
+        //Test 4 - Can remove any type of student from the roster if they're in there - nonResStudent
+        Roster test4 = new Roster();
+        test4.add(nonResStudent);
+        assertFalse(test4.remove(interStudent));
+        assertFalse(test4.remove(triStudent));
+        assertTrue(test4.remove(nonResStudent));
+        assertFalse(test4.remove(resStudent));
+        assertFalse(test4.remove(testStudent));
+
+
+        //Test 5 - Can remove any type of student from the roster if they're in there - triStudent
+        Roster test5 = new Roster();
+        test5.add(triStudent);
+        assertFalse(test5.remove(interStudent));
+        assertTrue(test5.remove(triStudent));
+        assertFalse(test5.remove(nonResStudent));
+        assertFalse(test5.remove(resStudent));
+        assertFalse(test5.remove(testStudent));
     }
 }
